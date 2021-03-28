40d1769ffaf2926a6b2d70cb2bfeb3ee9ec0e938
Ethanol <ethanolchennault@gmail.com>
2021-03-27 23:45:53 -0700

    Update __init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,46 +1,9 @@
-import importlib.util
-from pathlib import Path
-import inspect
 import json
 import os
-import sys
+from pathlib import Path
 
 import discord
 from discord.ext import commands
-
-
-def generate_cogs_dict():
-    """returns a dictionary of CogName: Cog items. No longer used but good to have around"""
-    dict = {}
-    cogs_dir = os.path.dirname(__file__)
-    cog_paths = [
-        os.path.join(root, file)
-        for root, _, files in os.walk(cogs_dir)
-        for file in files
-        if file.endswith("index.py")
-    ]
-
-    for cog_path in cog_paths:
-        cog_name = os.path.basename(os.path.dirname(cog_path))
-
-        spec = importlib.util.spec_from_file_location(cog_name, cog_path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[spec.name] = module
-        spec.loader.exec_module(module)
-
-        name, cog = [
-            class_
-            for class_ in inspect.getmembers(module, inspect.isclass)
-            if class_[1].__module__ == module.__name__
-            and str(type(class_[1])) == "<class 'discord.ext.commands.cog.CogMeta'>"
-        ][0]
-
-        dict[name] = cog
-
-    return dict
-
-
-# cogs_dict = generate_cogs_dict()
 
 
 def cogs_list():
